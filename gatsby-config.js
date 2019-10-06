cbc050343af0608f0960b6ef77960eea31cbda18
Artem Marochkanych <serartmar@gmail.com>
2019-10-06 22:07:21 +0300

    Add Preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,7 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-preact'
   ],
 }
